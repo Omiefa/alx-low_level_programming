eb364436056ac41518ebdd8e8ccbf3882b7bea36
Omiefa <bristolmelvin@yahoo.com>
2023-06-16 13:28:06 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints out the lower case alphabets
+ * Return - 0 if successful else 1
+ */
+
+int main(void)
+{
+	char lower_case_alp = 'a';
+
+	for (lower_case_alp = 'a'; lower_case_alp <= 'z'; lower_case_alp++)
+	{
+		putchar(lower_case_alp);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
