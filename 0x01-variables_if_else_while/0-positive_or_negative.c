65eed4c46bc09dc6c11a072af0694c4b1d702c12
Omiefa <bristolmelvin@yahoo.com>
2023-06-16 03:41:52 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /*more headers goes there*/
-
+#include <stdio.h>
 /*betty style doc for function main goes there */
 /**
  *  main - determines number type
@@ -35,4 +35,4 @@ int main(void)
 	}
 
 	return (0);
-
+}
