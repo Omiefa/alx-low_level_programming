b96e45c0f01b85f6e4723cbb3348fa6c6aa22e96
Omiefa <bristolmelvin@yahoo.com>
2023-06-16 02:55:46 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,38 @@
-#include <stdlib.h> 
-#include <time.h> 
-/* more headers goes there */ 
-  
-/* betty style doc for function main goes there */ 
-int main(void) 
+#include <stdlib.h>
+#include <time.h>
+/*more headers goes there*/
+
+/*betty style doc for function main goes there */
+/**
+ *  main - determines number type
+ *
+ *  Return: 0 if successful else 1
+ */
+int main(void)
 {
-	int n; 
-	
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n < 0) {
-		
+	if (n < 0)
+	{
 		printf("Number is Negative\n");
 	}
 
-	else if (n > 0 ) {
-	
+	else if (n > 0)
+	{
+
 		printf("Number is Positive\n");
-	
+
 	}
 
-	else {
+	else
+	{
 
 		printf("Number is Zero\n");
-	
+
 	}
 
 	return (0);
-}
+
