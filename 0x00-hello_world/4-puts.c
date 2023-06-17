96af7599d6ad5a93f2ab5e78533b16154918a81e
Omiefa <bristolmelvin@yahoo.com>
2023-06-17 08:18:05 -0700

    use puts function to print out a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - prints exactly "Programming is like building a multilingual puzzle
+ * Use the function puts not printf
+ * Return: if 0 successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
