a2452c1e15d10b923b37a0601dafb4113d65d977
Omiefa <bristolmelvin@yahoo.com>
2023-06-17 08:28:16 -0700

    use printf function to print out a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - print out with proper grammar, but the outcome is a piece of art,
+ * Use the function printf
+ * You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+ * Return: o for success
+ */
+int main(void)
+{
+	printf("out with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
