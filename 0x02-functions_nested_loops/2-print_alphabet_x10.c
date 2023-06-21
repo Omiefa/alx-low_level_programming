69c2bb8ffb5b3767f34d04f138b1d6a02d64f905
Omiefa <bristolmelvin@yahoo.com>
2023-06-21 16:44:06 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 # include "main.h"
 /**
- * main- prints 10 times the alphabet, in lowercase, followed by a new line
- *
- * Return: always 0
+ * print_alphabet_x10- Print ten times the alphabets in lowercase
+ * Return: Always 0 for success
  */
+
 void print_alphabet_x10(void)
 {
 	int i, j;
