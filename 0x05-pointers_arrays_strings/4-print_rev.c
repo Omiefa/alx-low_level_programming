c0067e1f5cfd87bf56042a1be17182972fbdfea8
Omiefa <bristolmelvin@yahoo.com>
2023-06-27 13:21:42 -0700

    prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+# include "main.h"
+# include <string.h>
+/**
+ *print_rev- prints a string, in reverse, followed by a new line
+ *@s: text strings
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int m;
+	int len = strlen(s);
+
+	for (m = len; m >= 0; m--)
+	{
+		_putchar(s[m]);
+	}
+	_putchar('\n');
+}
