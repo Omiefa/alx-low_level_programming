848267193dc85053ca904d5381698ca05dbe0273
Omiefa <bristolmelvin@yahoo.com>
2023-06-30 11:45:06 -0700

    prints the n times table, starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_times_table - print n times table
+ * @n: parameter
+ */
+void print_times_table(int n)
+{
+	int d, m, result;
+
+	if (n <= 15 && n >= 0)
+	for (d = 0; d <= n; d++)
+	{
+	_putchar('0');
+		for (m = 1; m <= n; m++)
+		{
+		_putchar(',');
+		_putchar(' ');
+		result = d * m;
+		if (result <= 99)
+			_putchar(' ');
+		if (result <= 9)
+			_putchar(' ');
+		if (result >= 100)
+		{
+		_putchar((result / 100) + '0');
+		_putchar(((result / 10) % 10) + '0');
+		}
+		else if (result <= 99 && result >= 10)
+		{
+		_putchar((result / 10) + '0');
+		}
+		_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
