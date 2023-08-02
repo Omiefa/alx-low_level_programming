93db1707f39f5e4aa4ecefe5efffd9e16957b310
Omiefa <bristolmelvin@yahoo.com>
2023-08-02 03:15:28 +0100

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 	const listint_t *temp = h;
 
-	count = 0;
 	while (temp != NULL)
 	{
-
 		printf("%d\n", temp->n);
-		temp = temp->next;
 		count++;
+		temp = temp->next;
 	}
 	return (count);
 }
