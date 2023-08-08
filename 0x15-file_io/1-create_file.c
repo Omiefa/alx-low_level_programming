dbc456f78ff6dd33af60e71b0125a91c0c2ebdf7
Omiefa <bristolmelvin@yahoo.com>
2023-08-08 01:53:36 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,15 +19,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
 		text_content = "";
 
-		while (text_content[i])
-		{
-			i++;
-			text = write(fd, text_content, i);
-		}
+	while (text_content[i])
+	{
+		i++;
+		text = write(fd, text_content, i);
 	}
+
 	if (text == -1)
 		return (-1);
 
