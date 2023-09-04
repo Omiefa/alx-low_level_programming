56bac84279ac801d032c501a9e13a9cb4b5dc4eb
Omiefa <bristolmelvin@yahoo.com>
2023-09-04 21:22:56 +0100

    program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+void error_file(int file_from, int file_to, char *av[]);
+
+/**
+  *main - rogram that copies the content of a file to another file
+  *@ac: argument count
+  *@av: argument vector
+  *Return: always 0 on success
+  */
+int main(int ac, char *av[])
+{
+	int file_from, file_to, erro_close;
+	ssize_t num_chars, new;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_fromvfile_to");
+		exit(97);
+	}
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, av);
+
+	num_chars = 1024;
+	while (num_chars == 1024)
+	{
+		num_chars = read(file_from, buffer, 1024);
+		if (num_chars == -1)
+			error_file(-1, 0, av);
+		new = write(file_to, buffer, num_chars);
+		if (new == -1)
+			error_file(0, -1, av);
+	}
+
+	erro_close = close(file_from);
+	if (erro_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	erro_close = close(file_to);
+	if (erro_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd     %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
+/**
+  *error_file- check if a file is opened
+  *@file_from: initial file to copy from and to check
+  *@file_to: destination file to copy to
+  *@av: argument vector
+  *Return: void
+  */
+
+void error_file(int file_from, int file_to, char *av[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write %s\n", av[2]);
+		exit(99);
+	}
+}
